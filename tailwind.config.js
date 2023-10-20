d6e6f80b2a98cc12f46345546137cb765bce684d
omkarkokil <nsjustice21@gmail.com>
2023-10-20 19:08:55 +0530

    taiwind fix  --ui

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ["class"],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        extend: {
+            colors: {
+                warning: "hsl(var(--warning))",
+                success: "hsl(var(--success))",
+                primary: "hsl(var(--primary))",
+                secondary: "hsl(var(--secondary))",
+                danger: "hsl(var(--danger))",
+            }
+        }
+    },
+    plugins: [require("tailwindcss-animate")],
+}
